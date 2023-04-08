cb938495f4b03498d689de8dcf2a018b3fa139e6
Vishal Bhardwaj <108397932+Vishabhardwaj@users.noreply.github.com>
2023-04-08 15:33:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const generateElement = document.getElementById("generate");
 const clipboardElement = document.getElementById("clipboard");
 
 // Random functions
-// fromCharCode: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
-// ASCII codes: https://www.w3schools.com/charsets/ref_html_ascii.asp
+
+
 const getRandomLower = () =>
   String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
